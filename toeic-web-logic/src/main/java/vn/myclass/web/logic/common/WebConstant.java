91944ce8819d73354513a7ea60616afa01b03202
truonglam <cntt.clc@gmail.com>
2017-05-14 18:12:27 +0700

    init project

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/logic/common/WebConstant.java
@@ -0,0 +1,8 @@
+package vn.myclass.web.logic.common;
+
+/**
+ * Created by Admin on 14/5/2017.
+ */
+public class WebConstant {
+
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+   <title>test project</title>
+</head>
+<body>
+<h1>test project</h1>
+</body>
+</html>
