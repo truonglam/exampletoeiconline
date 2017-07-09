8fe137f064fc69896c5c034e44156be6279094cd
truonglam <cntt.clc@gmail.com>
2017-07-09 08:22:29 +0700

    init test api

---
+++ toeic-api-test/src/main/java/test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Admin on 9/7/2017.
+ */
+public class test {
+}
---
+++ toeic-api-test/src/test/java/vn/myclass/api/test/ApiTest.java
@@ -0,0 +1,8 @@
+package vn.myclass.api.test;
+
+/**
+ * Created by Admin on 9/7/2017.
+ */
+public class ApiTest {
+
+}
