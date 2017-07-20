f0b4e1819367f3061d4c709630a8c56615b05011
truonglam <cntt.clc@gmail.com>
2017-07-20 17:14:31 +0700

    display tag

--- toeic-core-common/src/main/java/vn/myclass/core/common/constant/CoreConstant.java
+++ toeic-core-common/src/main/java/vn/myclass/core/common/constant/CoreConstant.java
@@ -4,6 +4,6 @@ package vn.myclass.core.common.constant;
  * Created by Admin on 10/6/2017.
  */
 public class CoreConstant {
-    public static final String SORT_ASC = "2";
-    public static final String SORT_DESC = "1";
+    public static final String SORT_ASC = "1";
+    public static final String SORT_DESC = "2";
 }
---
+++ toeic-core-web/src/main/java/vn/myclass/core/web/utils/RequestUtil.java
@@ -0,0 +1,35 @@
+package vn.myclass.core.web.utils;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.displaytag.tags.TableTagParameters;
+import org.displaytag.util.ParamEncoder;
+import vn.myclass.core.web.command.AbstractCommand;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Admin on 16/7/2017.
+ */
+public class RequestUtil {
+    public static void initSearchBean(HttpServletRequest request, AbstractCommand bean) {
+        if (bean != null) {
+            String sortExpression = request.getParameter(new ParamEncoder(bean.getTableId()).encodeParameterName(TableTagParameters.PARAMETER_SORT));
+            String sortDirection = request.getParameter(new ParamEncoder(bean.getTableId()).encodeParameterName(TableTagParameters.PARAMETER_ORDER));
+            String pageStr = request.getParameter(new ParamEncoder(bean.getTableId()).encodeParameterName(TableTagParameters.PARAMETER_PAGE));
+
+            Integer page = 1;
+            if (StringUtils.isNotBlank(pageStr)) {
+                try {
+                    page = Integer.valueOf(pageStr);
+                } catch (Exception e) {
+                    //ignore
+                }
+            }
+            bean.setPage(page);
+            bean.setSortDirection(sortDirection);
+            bean.setSortExpression(sortExpression);
+            bean.setFirstItem((bean.getPage() - 1) * bean.getMaxPageItems());
+        }
+    }
+}
