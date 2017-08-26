d13abad9edd6eec8735534d36878bb17cfb7977c
truonglam <cntt.clc@gmail.com>
2017-08-26 16:13:34 +0700

    commit

---
+++ toeic-core-serviceimpl/src/main/java/vn/myclass/core/service/impl/RoleServiceImpl.java
@@ -0,0 +1,28 @@
+package vn.myclass.core.service.impl;
+
+import vn.myclass.core.dao.RoleDao;
+import vn.myclass.core.daoimpl.RoleDaoImpl;
+import vn.myclass.core.dto.RoleDTO;
+import vn.myclass.core.persistence.entity.RoleEntity;
+import vn.myclass.core.service.RoleService;
+import vn.myclass.core.utils.RoleBeanUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Admin on 26/8/2017.
+ */
+public class RoleServiceImpl implements RoleService {
+    RoleDao roleDao = new RoleDaoImpl();
+
+    public List<RoleDTO> findAll() {
+        List<RoleEntity> entities = roleDao.findAll();
+        List<RoleDTO> dtos = new ArrayList<RoleDTO>();
+        for (RoleEntity item: entities) {
+            RoleDTO dto = RoleBeanUtil.entity2Dto(item);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+}
