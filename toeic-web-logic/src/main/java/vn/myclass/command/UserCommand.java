c9fcbd6fbf4de82797a94cd76bfc374534de1cff
truonglam <cntt.clc@gmail.com>
2017-08-26 16:17:58 +0700

    insert edit user management

--- toeic-web-logic/src/main/java/vn/myclass/command/UserCommand.java
+++ toeic-web-logic/src/main/java/vn/myclass/command/UserCommand.java
@@ -1,13 +1,17 @@
 package vn.myclass.command;
 
+import vn.myclass.core.dto.RoleDTO;
 import vn.myclass.core.dto.UserDTO;
 import vn.myclass.core.web.command.AbstractCommand;
+
+import java.util.List;
 
 /**
  * Created by Admin on 8/7/2017.
  */
 public class UserCommand extends AbstractCommand<UserDTO> {
     private String confirmPassword;
+    private List<RoleDTO> roles;
     public UserCommand() {
         this.pojo = new UserDTO();
     }
@@ -18,5 +22,13 @@ public class UserCommand extends AbstractCommand<UserDTO> {
 
     public void setConfirmPassword(String confirmPassword) {
         this.confirmPassword = confirmPassword;
+    }
+
+    public List<RoleDTO> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<RoleDTO> roles) {
+        this.roles = roles;
     }
 }
